cccbe415920caff450840b7f1eb65cc54b1db7d0
orbb1 <orbbmax@gmail.com>
2022-09-06 11:18:53 +0200

    working POC

--- background.js
+++ background.js
@@ -7,5 +7,9 @@ chrome.runtime.onInstalled.addListener(async () => {
 });
 
 chrome.contextMenus.onClicked.addListener((item) => {
-  console.log(item.linkUrl)
+  const suffix = 'http://localhost:3000/results'
+  const params = new URLSearchParams()
+  params.append('url', item.linkUrl)
+  const url = suffix + "?" + params.toString()
+  chrome.tabs.create({ url })
 });
